52be0aab6c33327b5d3195704c39a1f307808c8e
aochuka <ochukaamon@gmail.com>
2026-06-24 10:55:50 +0300

    fix: remove stray docker command accidentally pasted into sanitize.go stub

--- backend/pkg/sanitize/sanitize.go
+++ backend/pkg/sanitize/sanitize.go
@@ -1,7 +1,1 @@
 package sanitize
-docker run --name ganji-postgres \
-  -e POSTGRES_USER=student \
-  -e POSTGRES_PASSWORD=devpassword \
-  -e POSTGRES_DB=ganji \
-  -p 5432:5432 \
-  -d postgres:16
